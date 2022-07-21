9a5dd719a4d61aec12188e35f7e8168ec029bb38
Michael <102828476+HappyPanda6453@users.noreply.github.com>
2022-07-21 17:44:52 -0400

    Add files via upload

---
+++ ExampleREADME.md
@@ -0,0 +1,58 @@
+# Project Title
+
+  ![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/happypanda6453/readme-generator?style=flat&logo=appveyor) ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/happypanda6453/readme-generator?style=flat&logo=appveyor)
+  
+  Check out the badges hosted by [shields.io](https://shields.io/).
+  
+  
+  ## Description 
+  
+  *The what, why, and how:* 
+  
+  Project Description
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [License](#license)
+  
+  ## Installation
+  
+  *Steps required to install project and how to get the development environment running:*
+  
+  undefined
+  
+  ## Usage 
+  
+  *Instructions and examples for use:*
+  
+  undefined
+  
+  ## Contributing
+  
+  *If you would like to contribute it, you can follow these guidelines for how to do so.*
+  
+  undefined
+  
+  ## Tests
+  
+  *Tests for application and how to run them:*
+  
+  undefined
+  
+  ## License
+  
+  Mozilla Public License 2.0
+  
+  ---
+  
+  ## Questions?
+
+  <img src="https://avatars.githubusercontent.com/u/102828476?v=4" alt="HappyPanda6453" width="40%" />
+  
+  For any questions, please contact me with the information below:
+ 
+  GitHub: [@HappyPanda6453](https://api.github.com/users/HappyPanda6453)
+  
---
+++ index.js
@@ -0,0 +1,105 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+// Internal modules
+const api = require('./utils/api.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+// Inquirer prompts for userResponses
+const questions = [
+    {
+        type: 'input',
+        message: "What is your GitHub username?",
+        name: 'username',
+        default: 'happypanda6453',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please enter a valid GitHub username.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is the name of your GitHub repo?",
+        name: 'repo',
+        default: 'readme-generator',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid GitHub repo is required for a badge.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is the title of your project?",
+        name: 'title',
+        default: 'Project Title',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project title is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Write a description of your project.",
+        name: 'description',
+        default: 'Project Description',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project description is required.");
+            }
+            return true;
+        }
+    },
+    
+    {
+        type: 'list',
+        message: "Choose a license for your project.",
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        name: 'license'
+    }
+];
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("Success! Your README.md file has been generated")
+    });
+}
+
+const writeFileAsync = util.promisify(writeToFile);
+
+// Main function
+async function init() {
+    try {
+
+        // Prompt Inquirer questions
+        const userResponses = await inquirer.prompt(questions);
+        console.log("Your responses: ", userResponses);
+        console.log("Thank you for your responses! Fetching your GitHub data next...");
+    
+        // Call GitHub api for user info
+        const userInfo = await api.getUser(userResponses);
+        console.log("Your GitHub user info: ", userInfo);
+    
+        // Pass Inquirer userResponses and GitHub userInfo to generateMarkdown
+        console.log("Generating your README next...")
+        const markdown = generateMarkdown(userResponses, userInfo);
+        console.log(markdown);
+    
+        // Write markdown to file
+        await writeFileAsync('ExampleREADME.md', markdown);
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+init();
